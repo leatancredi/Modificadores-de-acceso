89c0913d8ceb10a55a4b8ab7def3548b4212de62
leandro <leatancredi@gmail.com>
2021-11-15 02:10:42 -0300

    Subiendo ejercico de modificadores de acceso

---
+++ src/paquete1/Clase1.java
@@ -0,0 +1,17 @@
+package paquete1;
+
+public class Clase1 {
+    //las clases publicas se pueden usar en cualquier entorno dentro o fuera
+    // de un paquete
+   public String atributoPublico = "valor atributo publico";
+   
+   public Clase1(){
+       System.out.println("constructor publico");
+   }
+   
+   public void metodoPublico(){
+       System.out.println("metodo publico");
+   }
+   
+   
+}
